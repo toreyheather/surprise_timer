39c51aca5be9c90cc4e99a872c9b835902fb77a4
Torey Leggett <victoriahleggett@gmail.com>
2018-09-08 16:51:10 -0400

    finished project

--- js/app.js
+++ js/app.js
@@ -1,5 +1,30 @@
+// ORIGINAL FUNCTION
+//
+//
+//const surpriseSection = document.getElementById('surprise');
+//
+//function showSurprise() {
+//    surpriseSection.textContent = '🎉 Surprise! 🎉';
+//}
+//
+//const randomTime = Math.random() * 4000;
+//
+//setTimeout(showSurprise, randomTime);
+//
+//ANONYMOUS FUNCTION
+//
+//const surpriseSection = document.getElementById('surprise');
+//
+//const randomTime = Math.random() * 4000;
+//
+//setTimeout(function () {
+//    surpriseSection.textContent = '🎉 Surprise! 🎉';
+//}, randomTime);
+//
+//ARROW FUNCTION
+//
 const surpriseSection = document.getElementById('surprise');
 
-function showSurprise() {
-    surpriseSection.textContent = '🎉 Surprise! 🎉';
-}
+const randomTime = Math.random() * 4000;
+
+setTimeout( () => surpriseSection.textContent = '🎉 Surprise! 🎉', randomTime);
